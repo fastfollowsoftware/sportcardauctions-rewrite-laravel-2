b9631a8137158a246340693fac7cba37436c3049
David Maksimov <dmaksi@gmail.com>
2023-09-19 12:58:40 -0500

    fix inertia progress indicator

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -8,8 +8,8 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-  title: title => `${title} - ${appName}`,
-  resolve: name => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
   setup({ el, App, props }) {
     const root = createRoot(el);
 
@@ -17,5 +17,6 @@ createInertiaApp({
   },
   progress: {
     color: '#4B5563',
+    includeCSS: true,
   },
 });
