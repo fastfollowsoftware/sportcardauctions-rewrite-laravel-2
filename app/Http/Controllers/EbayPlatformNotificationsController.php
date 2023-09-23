9a58be0f6160507c2bd802b97e783c41fed3475f
David Maksimov <dmaksi@gmail.com>
2023-09-23 19:09:23 -0400

    save raw content for ebay platform notifications

--- app/Http/Controllers/EbayPlatformNotificationsController.php
+++ app/Http/Controllers/EbayPlatformNotificationsController.php
@@ -4,23 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\EbayPlatformNotification;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use VaclavVanik\DomToArray\DomToArray;
 
 class EbayPlatformNotificationsController extends Controller
 {
     public function index(Request $request)
     {
-        Log::info("ebay platform notification: " . $request->getContent());
+        $ebayPlatformNotification = EbayPlatformNotification::create([
+            'raw_content' => $request->getContent(),
+            'headers' => $request->header(),
+        ]);
+
         $dom = new \DOMDocument();
         $dom->loadXML($request->getContent());
 
         $body = DomToArray::toArray($dom);
 
-        EbayPlatformNotification::create([
-            'headers' => $request->header(),
-            'body' => $body,
-        ]);
+        $ebayPlatformNotification->body = $body;
+        $ebayPlatformNotification->save();
+
 
         return response('ok');
     }
