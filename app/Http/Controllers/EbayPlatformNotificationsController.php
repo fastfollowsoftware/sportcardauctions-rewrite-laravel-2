02de6feca77f68b182e95d86dee2312fff01f0d8
David Maksimov <dmaksi@gmail.com>
2023-09-23 18:54:54 -0400

    log

--- app/Http/Controllers/EbayPlatformNotificationsController.php
+++ app/Http/Controllers/EbayPlatformNotificationsController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\EbayPlatformNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use VaclavVanik\DomToArray\DomToArray;
 
 class EbayPlatformNotificationsController extends Controller
 {
     public function index(Request $request)
     {
+        Log::info("ebay platform notification: " . $request->getContent());
         $dom = new \DOMDocument();
         $dom->loadXML($request->getContent());
 
